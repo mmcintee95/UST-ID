31375196ed33a5f702ad9b675a688db8dfb8e768
mmcintee95 <mcin8981@stthomas.edu>
2025-02-25 10:23:02 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-function toggleMenu() {
-    var menu = document.getElementById("menu");
-    menu.style.display = (menu.style.display === "block") ? "none" : "block";
-}
-
-function uploadProfilePic(event) {
-    var reader = new FileReader();
-    reader.onload = function(){
-        var img = document.getElementById("profile-pic");
-        img.src = reader.result;
-    };
-    reader.readAsDataURL(event.target.files[0]);
-}
-
 function toggleLostCard() {
     var lostCard = document.getElementById("lost-card").checked;
     if (lostCard) {
@@ -19,12 +5,4 @@ function toggleLostCard() {
     } else {
         alert("Your physical ID is now reactivated.");
     }
-}
-
-function showResources() {
-    document.getElementById("resources").style.display = "block";
-}
-
-function hideResources() {
-    document.getElementById("resources").style.display = "none";
 }
