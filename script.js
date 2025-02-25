f32df2df3cf9ce49e3c14da9c0168b884045db99
mmcintee95 <mcin8981@stthomas.edu>
2025-02-25 10:46:23 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleLostCard() {
-    var lostCard = document.getElementById("lost-card").checked;
-    if (lostCard) {
-        alert("Your physical ID is now locked. Use only the digital version.");
-    } else {
-        alert("Your physical ID is now reactivated.");
-    }
-}
